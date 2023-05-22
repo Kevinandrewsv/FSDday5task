68c2ff4e2d521a5bed3f4c030cc9888c1da64ffa
kevin andrews <kevinandrews001@gmail.com>
2023-05-22 07:24:42 +0530

    FSD day5 task

---
+++ FSDday5task2.js
@@ -0,0 +1,36 @@
+// Create your own resume data in JSON format
+
+let resume = {
+    name: "Kevin Andrews",
+    phoneNumber: 9994053302,
+    address: {
+        doorNo: 65,
+        street: "sivan kovil street",
+        panchayat: "Koradacheri",
+        taluk: "needamangalam",
+        district: "thiruvarur",
+        pincode: 613703,
+        state: "Tamil Nadu"
+    },
+    educationalQualification: {
+        SSLC: {
+            markPercentage: 89.9,
+            yearOfPassing: 2011,
+            institutionName: "Neelan maticulation higher secondary school, needamangalam",
+        },
+        HSC: {
+            markPercentage: 80.5,
+            yearOfPassing: 2013,
+            institutionName: "Neelan matriculation higher secondary school, needamangalam",
+        },
+        UG: {
+            degree: "Bachelor of Engineering",
+            discipline: "Mechanical Engineering",
+            CGPA: 7.6,
+            yearOfPassing: 2017,
+            institutionName: "jayam college of engineering and technology, dharmapuri",
+        }
+    },
+    yearsOfExperience: 0,
+    skills: ["HTML", "CSS", "JavaScript"]
+}
